246a6b44553f00fc5bf7c935fb114b49caf5b112
Sophia Barrett <sophia@yoursummit.media>
2021-11-26 15:39:58 -0700

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -73,15 +75,27 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 
 // CREATE a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'successfully added candidate',
+            data: body
+        });
+    });
+});
+
 
 // default response for requests that are not found
 app.use((req, res) => {
