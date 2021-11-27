6a7b3d02b3a69f91bd6b3696e18fe2a2d2a42473
Sophia Barrett <sophia@yoursummit.media>
2021-11-27 11:27:37 -0700

    add get routes for voters

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 
 router.use(require('./partyRoutes'));
 router.use(require('./candidateRoutes'));
+router.use(require('./voterRoutes'));
 
 module.exports = router;
