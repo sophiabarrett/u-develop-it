99ba0a47d1b78bab36ac9a53f5e975f68919f118
Sophia Barrett <sophia@yoursummit.media>
2021-11-26 17:53:11 -0700

    update routes to join with party data

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    party_id INTEGER,
     industry_connected BOOLEAN NOT NULL,
+    party_id INTEGER,
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
